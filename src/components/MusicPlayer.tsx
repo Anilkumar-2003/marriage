35c1932980af819554a92aa63691abb2f0316c36
Anilkumar-2003 <120651211+Anilkumar-2003@users.noreply.github.com>
2025-05-29 23:03:11 +0530

    Update MusicPlayer.tsx

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -24,7 +24,7 @@ const MusicPlayer: React.FC = () => {
 
   return (
     <div className="fixed bottom-4 right-4 z-50">
-      <audio ref={audioRef} src="public/1.mp3" autoPlay loop />
+      <audio ref={audioRef} src="/1.mp3" autoPlay loop />
       <button
         onClick={toggleMute}
         className="bg-white text-primary p-2 rounded-full shadow-lg hover:bg-primary hover:text-white transition"
